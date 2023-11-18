34157e660f95de03e483e064bfc8a596b05f8991
sitb157 <sitb157@gmail.com>
2023-11-18 22:31:17 +0900

    update-add function create context module

--- path_generation_ws/scripts/model.py
+++ path_generation_ws/scripts/model.py
@@ -1,5 +1,28 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
+def create_context_module(input_tensor):
+    # Define the dilation rates for each layer
+    dilation_rates = [(1, 1), (1, 1), (1, 1), (2, 4), (4, 8), (8, 12), (12, 16),
+                                  (16, 20), (20, 24), (24, 28), (28, 32), (32, 1), (1, 1)]
+    
+    # Input 
+    x = input_tensor
+    
+    for i, dilation in enumerate(dilation_rates):
+        # Add dilated convolution layers
+        x = layers.Conv2D(96, (3, 3), padding='same', dilation_rate=dilation)(x)
+        x = layers.ELU()(x)
+                
+        # Add spatial dropout to all but the last layer
+        if i < 12:
+            x = layers.SpatialDropout2D(0.20)(x)
+        
+        # Reduce the number of feature maps in the final layer
+        x = layers.Conv2D(16, (3, 3), padding='same', dilation_rate=dilation_rates[-1])(x)
+        x = layers.ELU()(x)
+    
+    # Return the model
+    return x
 
 def create_model():
     inputs = layers.Input(shape=(600, 600, 8))
@@ -15,22 +38,22 @@ def create_model():
     
     # 3rd, 4th, and 5th Convolutional layers
     for _ in range(3):
-    x = layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same')(x)
-    x = layers.ELU()(x)
+        x = layers.Conv2D(16, (3, 3), strides=(1, 1), padding='same')(x)
+        x = layers.ELU()(x)
     
     # Second Max Pooling
     x = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
     
     # Context Module (implement as described in Table 1)
-    # ... (context module implementation here)
+    x = create_context_module(x)
     
     # Max Unpooling
     x = tf.keras.layers.UpSampling2D(size=(2, 2))(x)
     
     # 10th, 11th, and 12th Convolutional layers
     for filters in [16, 16, 8]:
-    x = layers.Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(x)
-    x = layers.ELU()(x)
+        x = layers.Conv2D(filters, (3, 3), strides=(1, 1), padding='same')(x)
+        x = layers.ELU()(x)
     
     # Second Max Unpooling
     x = tf.keras.layers.UpSampling2D(size=(2, 2))(x)
@@ -47,7 +70,9 @@ def create_model():
     outputs = layers.Softmax()(x)
     model = models.Model(inputs=inputs, outputs=outputs)
 
+    # Print model summary
+    model.summary()
     return model
 
-model = create_model()
-model.summary()
+if __name__ == "__main__":
+    model = create_model()
